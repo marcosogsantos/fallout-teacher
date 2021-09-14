07106ab9095419859cea8200cd0b33a605fd2dd4
marcosogsantos <marcos_santos100@icloud.com>
2021-09-14 20:38:06 -0300

    fix: Make loop.py compatible with linux

--- loop.py
+++ loop.py
@@ -2,13 +2,24 @@ import time
 import random 
 import sys
 import subprocess
-
+import os
+import win32gui
+def windowEnumerationHandler(hwnd, top_windows):
+    top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+    
 while True:
-    # os.system("start cmd /k openTeacher.py")
-    # os.system("start http://localhost:3000")
     url = "http://localhost:3000"
     if sys.platform=='win32':
-        os.startfile(url)
+        os.system("start http://localhost:3000")
+        results = []
+        top_windows = []
+        win32gui.EnumWindows(windowEnumerationHandler, top_windows)
+        for i in top_windows:
+            if "chrome" in i[1].lower():
+                win32gui.ShowWindow(i[0],5)
+                win32gui.SetForegroundWindow(i[0])
+                break
+        
     elif sys.platform=='darwin':
         subprocess.Popen(['open', url])
     else:
